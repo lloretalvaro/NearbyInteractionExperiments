9893177f25b73a79a15457f4cbe4c320bada3e7a
lloretalvaro <alvarolloretlopez@gmail.com>
2023-11-23 22:20:16 +0100

    initial app

---
+++ NearbyInteractionExperiments/NearbyInteractionExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyInteractionExperimentsApp.swift
+//  NearbyInteractionExperiments
+//
+//  Created by Alvaro Lloret Lopez on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NearbyInteractionExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
